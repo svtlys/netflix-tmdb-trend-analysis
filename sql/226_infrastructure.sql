739742417088b189bcb7cd3ee45ffc964aa7a613
sangahlee0 <sangahlee82@gmail.com>
2026-04-29 15:34:48 -0700

    change the sql names, removed parts for dbt instead

--- sql/226_infrastructure.sql
+++ sql/226_infrastructure.sql
@@ -1,36 +1,30 @@
-DROP TABLE IF EXISTS USER_DB_COBRA.PUBLIC.NETFLIX_STAGING_ZONE;
-DROP TABLE IF EXISTS USER_DB_COBRA.PUBLIC.NETFLIX_TITLES;
-DROP TABLE IF EXISTS USER_DB_COBRA.PUBLIC.TMDB_GENRES;
-DROP TABLE IF EXISTS USER_DB_COBRA.PUBLIC.TMDB_TRENDING;
+DROP TABLE IF EXISTS USER_DB_COBRA.RAW.NETFLIX_TITLES;
+DROP TABLE IF EXISTS USER_DB_COBRA.RAW.TMDB_GENRES;
+DROP TABLE IF EXISTS USER_DB_COBRA.RAW.TMDB_TRENDING;
 
 -- Standard Schema for Team
 CREATE SCHEMA IF NOT EXISTS USER_DB_COBRA.RAW;
 CREATE SCHEMA IF NOT EXISTS USER_DB_COBRA.ANALYTICS;
 CREATE SCHEMA IF NOT EXISTS USER_DB_COBRA.DASHBOARD;
 
--- Temporary landing zone for the raw TABLES
-CREATE OR REPLACE TABLE USER_DB_COBRA.RAW.NETFLIX_STAGING_ZONE (
+-- Create Stage
+CREATE STAGE IF NOT EXISTS USER_DB_COBRA.RAW.NETFLIX_STAGE
+    FILE_FORMAT = (
+        TYPE = 'CSV'
+        SKIP_HEADER = 1
+        FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    );
+
+-- Create RAW table for Netflix titles
+CREATE OR REPLACE TABLE USER_DB_COBRA.RAW.NETFLIX_TITLES (
     show_id STRING,
     type STRING,
     title STRING,
     date_added STRING,
     release_year INT,
     rating STRING,
-    duration STRING,
-    listed_in STRING, -- The messy comma version
-    country STRING
-);
-
--- Create RAW table for Netflix titles
-CREATE OR REPLACE TABLE USER_DB_COBRA.RAW.NETFLIX_TITLES (
-    show_id STRING,
-    type STRING,      -- Used to filter 'Movie' later
-    title STRING,
-    date_added STRING,
-    release_year INT,
-    rating STRING,
-    duration INT,
-    listed_in STRING, -- comma-separated Genres
+    duration STRING,    -- dbt will cast this to INT
+    listed_in STRING,   -- raw comma-separated, dbt will flatten
     country STRING
 );
 
@@ -53,7 +47,21 @@ CREATE OR REPLACE TABLE USER_DB_COBRA.RAW.TMDB_GENRES (
     genre_name STRING
 );
 
+-- Create Dashboard
+CREATE OR REPLACE TABLE USER_DB_COBRA.DASHBOARD.FINAL_NETFLIX_MOVIES (
+    show_id STRING,
+    type STRING,
+    title STRING,
+    date_added STRING,
+    release_year INT,
+    rating STRING,
+    duration INT,      
+    listed_in STRING, 
+    country STRING
+);
+
 -- This allows anyone in the account to see and use your database 
 GRANT USAGE ON DATABASE USER_DB_COBRA TO ROLE PUBLIC;
 GRANT USAGE ON ALL SCHEMAS IN DATABASE USER_DB_COBRA TO ROLE PUBLIC;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA USER_DB_COBRA.RAW TO ROLE PUBLIC;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA USER_DB_COBRA.DASHBOARD TO ROLE PUBLIC;

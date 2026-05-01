96e17d2492b64d5cb811110e90f13a4b8749553b
Lyra <ananyayallapragada@gmail.com>
2026-05-01 11:35:58 -0700

    Loading into tmdb genre and trending tables

--- airflow/dags/tmdb_pipeline.py
+++ airflow/dags/tmdb_pipeline.py
@@ -4,7 +4,6 @@ from airflow.models import Variable
 from datetime import datetime
 import requests
 import pandas as pd
-import snowflake.connector
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.hooks.base import BaseHook
 
@@ -40,6 +39,19 @@ def extract_tmdb_data():
     df.to_csv("/tmp/tmdb_data.csv", index=False)
 
     print("TMDb realtime ingestion complete!")
+
+def extract_tmdb_genres():
+    API_KEY = Variable.get("tmdb_api_key")
+
+    url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={API_KEY}"
+    data = requests.get(url).json()
+
+    df = pd.DataFrame(data["genres"]).rename(columns={
+        "id": "genre_id",
+        "name": "genre_name"
+    })
+
+    df.to_csv("/tmp/tmdb_genres.csv", index=False)
 
 def load_tmdb():
     hook = SnowflakeHook(snowflake_conn_id="conn")
@@ -78,15 +90,66 @@ def load_tmdb():
         """)
 
         cur.execute("""
-            COPY INTO TMDB_TRENDING
-            FROM @TMDB_STAGE
-            FILE_FORMAT = (
-                TYPE = 'CSV'
-                SKIP_HEADER = 1
-                FIELD_OPTIONALLY_ENCLOSED_BY = '"'
-            )
-            FORCE = TRUE;
+    COPY INTO TMDB_TRENDING
+    FROM @TMDB_STAGE/tmdb_data.csv
+    FILE_FORMAT = (
+        TYPE = 'CSV'
+        SKIP_HEADER = 1
+        FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    )
+    FORCE = TRUE;
+""")
+
+    finally:
+        cur.close()
+        conn.close()
+
+def load_tmdb_genres():
+    hook = SnowflakeHook(snowflake_conn_id="conn")
+    conn = hook.get_conn()
+    cur = conn.cursor()
+
+    airflow_conn = BaseHook.get_connection("conn")
+    extra = airflow_conn.extra_dejson
+
+    warehouse = extra.get("warehouse")
+    database = extra.get("database")
+    schema = extra.get("schema") or airflow_conn.schema
+
+    try:
+        if warehouse:
+            cur.execute(f"USE WAREHOUSE {warehouse}")
+        if database:
+            cur.execute(f"USE DATABASE {database}")
+        if schema:
+            cur.execute(f"USE SCHEMA {schema}")
+
+            cur.execute("""
+    CREATE STAGE IF NOT EXISTS TMDB_STAGE
+    FILE_FORMAT = (
+        TYPE = 'CSV'
+        SKIP_HEADER = 1
+        FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    );
+""")
+
+        cur.execute("""
+            PUT file:///tmp/tmdb_genres.csv
+            @TMDB_STAGE
+            AUTO_COMPRESS=TRUE
+            OVERWRITE=TRUE;
         """)
+
+        cur.execute("""
+    COPY INTO TMDB_GENRES
+    FROM @TMDB_STAGE/tmdb_genres.csv
+    FILE_FORMAT = (
+        TYPE = 'CSV'
+        SKIP_HEADER = 1
+        FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    )
+    FORCE = TRUE;
+""")
 
     finally:
         cur.close()
@@ -107,6 +170,16 @@ with DAG(
     load_task = PythonOperator(
     task_id="load_tmdb",
     python_callable=load_tmdb
-)
+    )
+    extract_genres_task = PythonOperator(
+        task_id="extract_tmdb_genres",
+        python_callable=extract_tmdb_genres
+    )
 
-extract_task >> load_task
+    load_genres_task = PythonOperator(
+        task_id="load_tmdb_genres",
+        python_callable=load_tmdb_genres
+    )
+
+    extract_task >> load_task
+    extract_genres_task >> load_genres_task
